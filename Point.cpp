9de79a4a14c4c183f9082fea3ed97cc7cded8cdc
Gaspar Jimenez <oficialgaspii@gmail.com>
2023-06-04 22:27:43 -0400

    Constructores del Struct

---
+++ Point.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h> //Biblioteca donde se encuentra la función cout
+#include "Point.h"
+#include "Node.h"
+using namespace std;  //uso del espacio de nombre 
+
+Point::Point(int _x, int _y){
+    x = _x;
+    y = _y;
+}
+
+Point::Point(){
+    x = 0;
+    y = 0;
+}
