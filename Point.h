18042b8178ac20a8e5d873bb0e949573d3d18406
Gaspar Jimenez <oficialgaspii@gmail.com>
2023-06-04 22:21:19 -0400

    Clase Point(punto) que guardara las coordenadas

---
+++ Point.h
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> //Biblioteca donde se encuentra la función cout
+using namespace std;  //uso del espacio de nombre 
+
+struct Point
+{
+    int x;
+    int y;
+    Point(int _x,int _y);
+    Point();
+};
+
+
